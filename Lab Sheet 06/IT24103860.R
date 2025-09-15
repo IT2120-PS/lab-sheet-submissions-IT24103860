c61a7786ea4ba108664666915bcb02075fe379ac
IT24103860 <IT24103860@my.sliit.lk>
2025-09-15 15:34:05 +0530

    Labsheet 06

---
+++ Lab Sheet 06/IT24103860.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\utente\\Desktop\\IT24103860")
+
+# Exercise
+
+# Q-1
+#Binomial Distribution
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+# Q-2
+# X - The number of calls receives per hour
+# Poisson Distribution
+dpois(15, 12)
