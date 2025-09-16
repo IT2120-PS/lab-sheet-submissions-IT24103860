5e7177562c13cbf9fc64c830e28dc195600465ec
IT24103860 <IT24103860@my.sliit.lk>
2025-09-16 22:46:59 +0530

    Labsheet 07

---
+++ Lab Sheet 07/IT24103860.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\utente\\Desktop\\IT24103860")
+
+#Exercise 1
+
+#Q1
+
+punif(24,min = 0, max = 40, lower.tail = TRUE) - punif(10,min = 0, max = 40, lower.tail = TRUE)
+
+
+# Q2
+
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+
+# Q3
+
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+qnorm(0.95, mean = 100, sd = 15)
